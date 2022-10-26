994f0b40d88c88a2e2142141bf5e2a26cce8c64a
ilya.frontman <ilya.frontman@yandex.ru>
2022-10-26 19:47:57 +0300

    update source files: example assets and templates

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions and definitions
  *
@@ -13,31 +14,32 @@
  *
  * @since 1.0.0
  */
-define( 'NADRYV_PRODUCTION_VERSION', wp_get_theme()->get( 'Version' ) );
+define('NADRYV_PRODUCTION_VERSION', wp_get_theme()->get('Version'));
 
 /**
  * Add theme support.
  */
-function nadryv_production_setup() {
-	add_theme_support( 'wp-block-styles' );
-	add_editor_style( './assets/css/style-shared.min.css' );
+function nadryv_production_setup()
+{
+	add_theme_support('wp-block-styles');
+	add_editor_style('./assets/css/style-shared.min.css');
 
 	/*
 	 * Load additional block styles.
 	 * See details on how to add more styles in the readme.txt.
 	 */
-	$styled_blocks = [ 'button', 'file', 'quote', 'search' ];
-	foreach ( $styled_blocks as $block_name ) {
+	$styled_blocks = ['button', 'file', 'quote', 'search'];
+	foreach ($styled_blocks as $block_name) {
 		$args = array(
 			'handle' => "nadryv-production-$block_name",
-			'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.min.css" ),
-			'path'   => get_theme_file_path( "assets/css/blocks/$block_name.min.css" ),
+			'src'    => get_theme_file_uri("assets/css/blocks/$block_name.min.css"),
+			'path'   => get_theme_file_path("assets/css/blocks/$block_name.min.css"),
 		);
 		// Replace the "core" prefix if you are styling blocks from plugins.
-		wp_enqueue_block_style( "core/$block_name", $args );
+		wp_enqueue_block_style("core/$block_name", $args);
 	}
 }
-add_action( 'after_setup_theme', 'nadryv_production_setup' );
+add_action('after_setup_theme', 'nadryv_production_setup');
 
 /**
  * Enqueue the CSS files.
@@ -46,7 +48,8 @@ add_action( 'after_setup_theme', 'nadryv_production_setup' );
  *
  * @return void
  */
-function nadryv_production_styles() {
+function nadryv_production_styles()
+{
 	wp_enqueue_style(
 		'nadryv-production-style',
 		get_stylesheet_uri(),
@@ -55,15 +58,15 @@ function nadryv_production_styles() {
 	);
 	wp_enqueue_style(
 		'nadryv-production-shared-styles',
-		get_theme_file_uri( 'assets/css/style-shared.min.css' ),
+		get_theme_file_uri('assets/css/style-shared.min.css'),
 		[],
 		NADRYV_PRODUCTION_VERSION
 	);
 }
-add_action( 'wp_enqueue_scripts', 'nadryv_production_styles' );
+add_action('wp_enqueue_scripts', 'nadryv_production_styles');
 
 // Block style examples.
-require_once get_theme_file_path( 'inc/register-block-styles.php' );
+require_once get_theme_file_path('inc/register-block-styles.php');
 
 // Block pattern helper for the privacy policy.
-require_once get_theme_file_path( 'inc/block-pattern-helper.php' );
+require_once get_theme_file_path('inc/block-pattern-helper.php');
