971efa3b59b3beba92dda683d749c48f97dcdbfd
Ilya Fadeev <ilya.frontman@yandex.ru>
2022-10-31 01:22:42 +0300

    add ttf2woff and include font tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require('gulp'),
 	clean = require('gulp-clean'),
 	concatCss = require('gulp-concat-css'),
 	cssnano = require('gulp-cssnano'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	ttf2woff = require('gulp-ttf2woff');
 
 gulp.task('watch', function () {
 	gulp
@@ -12,8 +13,10 @@ gulp.task('watch', function () {
 			gulp.series(
 				'clean-shared',
 				'clean-blocks',
+				'clean-custom-fonts',
 				'minify-shared',
-				'minify-blocks'
+				'minify-blocks',
+				'minify-custom-fonts'
 			)
 		);
 });
@@ -36,12 +39,36 @@ gulp.task('clean-blocks', function () {
 		.pipe(clean());
 });
 
+gulp.task('ttf2woff', function () {
+	return gulp
+		.src(['assets/fonts/*.ttf'])
+		.pipe(ttf2woff())
+		.pipe(gulp.dest('assets/fonts/'));
+});
+
 gulp.task('minify-shared', function () {
 	return gulp
 		.src('assets/css/*.css')
 		.pipe(concatCss('style-shared.min.css'))
 		.pipe(cssnano())
 		.pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('clean-custom-fonts', function () {
+	return gulp
+		.src('assets/fonts/custom-fonts.min.css', {
+			read: false,
+			allowEmpty: true,
+		})
+		.pipe(clean());
+});
+
+gulp.task('minify-custom-fonts', function () {
+	return gulp
+		.src('assets/fonts/custom-fonts.css')
+		.pipe(concatCss('custom-fonts.min.css'))
+		.pipe(cssnano({ discardUnused: false }))
+		.pipe(gulp.dest('assets/fonts/'));
 });
 
 gulp.task('minify-blocks', function () {
@@ -54,5 +81,13 @@ gulp.task('minify-blocks', function () {
 
 gulp.task(
 	'default',
-	gulp.series('clean-shared', 'clean-blocks', 'minify-shared', 'minify-blocks')
+	gulp.series(
+		'ttf2woff',
+		'clean-shared',
+		'clean-blocks',
+		'clean-custom-fonts',
+		'minify-shared',
+		'minify-blocks',
+		'minify-custom-fonts'
+	)
 );
