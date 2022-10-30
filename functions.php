0f7f66e67e6789358005fae78624c63d6e80275c
Ilya Fadeev <ilya.frontman@yandex.ru>
2022-10-31 01:29:09 +0300

    update namespaces

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package nadryv-production
+ * @package wp-block-theme
  * @since 1.0.0
  */
 
@@ -14,12 +14,12 @@
  *
  * @since 1.0.0
  */
-define('NADRYV_PRODUCTION_VERSION', wp_get_theme()->get('Version'));
+define('WP_BLOCK_THEME_STARTER_VERSION', wp_get_theme()->get('Version'));
 
 /**
  * Add theme support.
  */
-function nadryv_production_setup()
+function wp_block_theme_setup()
 {
 	add_theme_support('wp-block-styles');
 	add_editor_style('./assets/css/style-shared.min.css');
@@ -31,7 +31,7 @@ function nadryv_production_setup()
 	$styled_blocks = ['button', 'file', 'quote', 'search'];
 	foreach ($styled_blocks as $block_name) {
 		$args = array(
-			'handle' => "nadryv-production-$block_name",
+			'handle' => "wp-block-theme-$block_name",
 			'src'    => get_theme_file_uri("assets/css/blocks/$block_name.min.css"),
 			'path'   => get_theme_file_path("assets/css/blocks/$block_name.min.css"),
 		);
@@ -39,7 +39,7 @@ function nadryv_production_setup()
 		wp_enqueue_block_style("core/$block_name", $args);
 	}
 }
-add_action('after_setup_theme', 'nadryv_production_setup');
+add_action('after_setup_theme', 'wp_block_theme_setup');
 
 /**
  * Enqueue the CSS files.
@@ -48,25 +48,36 @@ add_action('after_setup_theme', 'nadryv_production_setup');
  *
  * @return void
  */
-function nadryv_production_styles()
+function wp_block_theme_styles()
 {
 	wp_enqueue_style(
-		'nadryv-production-style',
+		'wp-block-theme-style',
 		get_stylesheet_uri(),
 		[],
-		NADRYV_PRODUCTION_VERSION
+		WP_BLOCK_THEME_STARTER_VERSION
 	);
 	wp_enqueue_style(
-		'nadryv-production-shared-styles',
+		'wp-block-theme-shared-styles',
 		get_theme_file_uri('assets/css/style-shared.min.css'),
 		[],
-		NADRYV_PRODUCTION_VERSION
+		WP_BLOCK_THEME_STARTER_VERSION
+	);
+
+	wp_enqueue_style(
+		'wp-block-theme-custom-fonts',
+		get_theme_file_uri('/assets/fonts/custom-fonts.min.css'),
+		[],
+		WP_BLOCK_THEME_STARTER_VERSION
 	);
 }
-add_action('wp_enqueue_scripts', 'nadryv_production_styles');
+add_action('wp_enqueue_scripts', 'wp_block_theme_styles');
 
 // Block style examples.
 require_once get_theme_file_path('inc/register-block-styles.php');
 
 // Block pattern helper for the privacy policy.
 require_once get_theme_file_path('inc/block-pattern-helper.php');
+
+// Load custom-fonts
+include_once get_theme_file_path('hooks/load-fonts.php');
+add_action('wp_head', 'load_web_fonts');
